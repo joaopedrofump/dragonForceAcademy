8bb72be6406517a6b00fa835c5bbe4524ad7a9c0
joaopedrofump <joaofurriel@wifi-interno.fe.up.pt>
2016-10-12 17:47:10 +0100

    added Classes with athlete

---
+++ Classes/Athlete.cpp
@@ -0,0 +1,9 @@
+//
+//  Athlete.cpp
+//  DragonForceAcadamy
+//
+//  Created by Joao Furriel on 12/10/16.
+//  Copyright © 2016 João Furriel Pinheiro. All rights reserved.
+//
+
+#include "Athlete.hpp"
---
+++ Classes/Athlete.hpp
@@ -0,0 +1,14 @@
+//
+//  Athlete.hpp
+//  DragonForceAcadamy
+//
+//  Created by Joao Furriel on 12/10/16.
+//  Copyright © 2016 João Furriel Pinheiro. All rights reserved.
+//
+
+#ifndef Athlete_hpp
+#define Athlete_hpp
+
+#include <stdio.h>
+
+#endif /* Athlete_hpp */
