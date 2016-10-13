ccc849bd0428223272ba02b4076282566a1921ff
joaopedrofump <joaofurriel@MBP-de-Joao-7.lan>
2016-10-13 13:19:05 +0100

    Data e Worker

---
+++ Classes/Worker.cpp
@@ -0,0 +1,9 @@
+//
+//  Worker.cpp
+//  DragonForceAcadamy
+//
+//  Created by Joao Furriel on 13/10/16.
+//  Copyright © 2016 João Furriel Pinheiro. All rights reserved.
+//
+
+#include "Worker.hpp"
---
+++ Classes/Worker.hpp
@@ -0,0 +1,26 @@
+//
+//  Worker.hpp
+//  DragonForceAcadamy
+//
+//  Created by Joao Furriel on 13/10/16.
+//  Copyright © 2016 João Furriel Pinheiro. All rights reserved.
+//
+
+#ifndef Worker_hpp
+#define Worker_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class Worker {
+    
+public:
+    
+    string name;
+    
+    
+};
+
+#endif /* Worker_hpp */
