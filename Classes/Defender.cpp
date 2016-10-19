56eff060020a293765c0ed43c6afdb41ebfe1d96
joaopedrofump <joaofurriel@MBP-de-Joao-7.lan>
2016-10-19 19:33:44 +0100

    added defender

---
+++ Classes/Defender.cpp
@@ -0,0 +1,9 @@
+//
+//  Defender.cpp
+//  DragonForceAcadamy
+//
+//  Created by Joao Furriel on 19/10/16.
+//  Copyright © 2016 João Furriel Pinheiro. All rights reserved.
+//
+
+#include "Defender.hpp"
---
+++ Classes/Defender.hpp
@@ -0,0 +1,14 @@
+//
+//  Defender.hpp
+//  DragonForceAcadamy
+//
+//  Created by Joao Furriel on 19/10/16.
+//  Copyright © 2016 João Furriel Pinheiro. All rights reserved.
+//
+
+#ifndef Defender_hpp
+#define Defender_hpp
+
+#include <stdio.h>
+
+#endif /* Defender_hpp */
