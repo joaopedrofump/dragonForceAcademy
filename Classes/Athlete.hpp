2db95b54e5019e2398d1232586890ad5c0ab130f
joaopedrofump <joaofurriel@MBP-de-Joao-7.lan>
2016-10-20 22:46:13 +0100

    pedro new Class ECG

--- Classes/Athlete.hpp
+++ Classes/Athlete.hpp
@@ -13,28 +13,6 @@
 #include <iostream>
 #include "Worker.hpp"
 
-<<<<<<< HEAD
-class ECG {
-private:
-    bool resultado;
-public:
-    ECG(bool resultado) {this->resultado=resultado;};
-    ECG& setResultado(bool resultado){this->resultado = resultado; return *this;};
-    bool getResultado() const {return this->resultado;};
-};
-
-class Athlete : public Worker {
-    
-protected:
-    unsigned char height;
-    ECG* ecg; //apontador para ECG quando houver classe
-
-public:
-    Athlete(string name, Date birthdate, unsigned char height);
-    ~Athlete();
-    virtual Position getPosition() const = 0;
-    void addECG(bool resultado);
-=======
 class Athlete : public Worker {
     
 private:
@@ -43,7 +21,7 @@ private:
 public:
     Athlete(string name, Date birthdate, unsigned char height);
     virtual Position getPosition() const = 0;
->>>>>>> 62fb5076ee9c11070ff7dda2f7f367bbba09d115
+
 };
 
 #endif /* Athlete_hpp */
